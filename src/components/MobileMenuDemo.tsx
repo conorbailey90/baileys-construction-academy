db75b866562e9009534bb64b1fba6cc0d731b428
Conor Bailey <conorbailey@Conors-MacBook-Pro.local>
2024-08-31 17:45:57 +0100

    key added to mobile menu mapping

--- src/components/MobileMenuDemo.tsx
+++ src/components/MobileMenuDemo.tsx
@@ -97,7 +97,7 @@ const MobileMenuDemo = () => {
                             <AccordionTrigger className='font-bold text-[1.44rem] hover:no-underline'>{link.name}</AccordionTrigger>
                             {link.subMenu.map(link => (
                                 <AccordionContent key={link.name}>
-                                    <Link  className='font-bold text-lg pl-[1rem] hover:text-[rgb(252,186,3)]' onClick={toggleMenu} href={link.href}>
+                                    <Link className='font-bold text-lg pl-[1rem] hover:text-[rgb(252,186,3)]' onClick={toggleMenu} href={link.href}>
                                         {link.name}
                                     </Link>
                                 </AccordionContent>
@@ -107,7 +107,7 @@ const MobileMenuDemo = () => {
                 </li>
             
                 :
-                <li key={idx} onClick={toggleMenu}  style={{borderBottom: '1px solid rgba(255, 255, 255, 255)'}} className='w-[100%]'>
+                <li key={idx} onClick={toggleMenu} style={{borderBottom: '1px solid rgba(255, 255, 255, 255)'}} className='w-[100%]'>
                     <Link href={link.href}>
                         <span className='inline-block cursor-pointer font-bold text-[1.44rem] py-[1rem] w-[100%] hover:text-[rgb(252,186,3)]'> 
                             {link.name}
