46dc34c382668cfe7f28f565c8d278c14516fc3f
Conor Bailey <conorbailey@Conors-MacBook-Pro.local>
2024-09-20 07:29:46 +0100

    SEO metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,38 @@ import { ThemeProvider } from "@/components/ThemeProvider"
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+// export const metadata: Metadata = {
+//   title: "Bricklaying and Plastering Courses - Essex",
+//   description: "Build Your Future with Expert Training in Bricklaying & Plastering"
+// };
+
+export const metadata = {
   title: "Bricklaying and Plastering Courses - Essex",
-  description: "Build Your Future with Expert Training in Bricklaying & Plastering"
+  description: "Build Your Future with Expert Training in Bricklaying & Plastering",
+  keywords: "bricklaying courses, plastering courses, Essex, construction skills, trade training",
+  openGraph: {
+    title: "Bricklaying & Plastering Courses in Essex | Hands-On Construction Training",
+    description: "Enroll in professional bricklaying and plastering courses in Essex. Get practical skills from experienced instructors and start your construction career today!",
+    url: "https://bricklayingandplasteringcoursesessex.com",
+    images: [
+      {
+        url: "/public/brick.jpg", // Replace with your image URL
+        width: 1200,
+        height: 630,
+        alt: "Bricklaying and Plastering Courses in Essex"
+      }
+    ],
+    locale: "en_GB",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bricklaying & Plastering Courses in Essex",
+    description: "Learn essential bricklaying and plastering skills with our hands-on courses in Essex. Book a course now to boost your construction career!",
+    images: ["/public/bricklay.jpg"], // Replace with your image URL
+  },
 };
+
 
 export default function RootLayout({
   children,
