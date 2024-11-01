30385633c4f87bc80d5b6089b5f85c861700f2f6
Conor Bailey <conorbailey@Conors-MacBook-Pro.local>
2024-11-01 08:57:35 +0000

    Favicon added

--- src/app/actions/sendEmail.ts
+++ src/app/actions/sendEmail.ts
@@ -17,7 +17,7 @@ import { EmailTemplate } from '@/components/email-template';
         const resend = new Resend(process.env.RESEND_API_KEY);
         const {data, error} = await resend.emails.send({
             from: 'Bricklaying & Plastering Courses Essex <onboarding@resend.dev>',
-            to: ['conbailey90@gmail.com', 'bvadherconsultancy@gmail.com', 'bhavin@paulbailey.info', 'info@paulbailey.info'],
+            to: ['conbailey90@gmail.com', 'bvadherconsultancy@gmail.com', 'bhavin@paulbailey.info', 'info@paulbailey.info', 'bricklayingandplasteringessex@gmail.com'],
             subject: 'Bricklaying and Plastering Courses Essex - New Query',
             react: EmailTemplate({ name, email, phone, message }), // Ensure this returns a valid React element
         });
